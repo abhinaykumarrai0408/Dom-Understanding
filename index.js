67f3234d32491994c33a044997ee2d48bc1fa23e
Abhinay Rai <abhirai0408@gmail.com>
2025-06-27 13:43:01 +0530

    querySelector & querySelectorAll Methods

--- index.js
+++ index.js
@@ -42,3 +42,50 @@ lastItem[4].style.color = "blue"
 for (let i = 0; i < lastItem.length; i++){
     lastItem[i].style.fontStyle = "italic";
 }
+
+
+
+
+// querySelector & querySelectorAll Methods
+
+// Write the code as shown in the video below:
+const mainheading = document.querySelector('#main-heading');
+mainheading.style.textAlign = "right"
+mainheading.style.padding = "10px"
+
+const allFruits = document.querySelector('.fruits ');
+allFruits.style.backgroundColor = "gray";
+allFruits.style.padding = "30px"
+allFruits.style.margin = "30px"
+allFruits.style.width = "50%"
+allFruits.style.borderRadius = "5px";
+allFruits.style.listStyleType = "none"
+
+
+// Write answer to the questions asked below:
+
+const fruitBasket = document.querySelector('#basket-heading');
+fruitBasket.style.color = "brown"
+fruitBasket.style.marginLeft = "30px"
+
+
+
+
+const allItem = document.querySelectorAll('.fruit')
+for (let i = 0; i < allItem.length; i++){  
+    allItem[i].style.backgroundColor = "white"
+    allItem[i].style.padding = "5px"
+    allItem[i].style.borderRadius = "5px"
+    allItem[i].style.margin = "5px"
+}
+
+
+const fruits = document.querySelectorAll('.fruits li');
+
+for (let i = 0; i < fruits.length; i++) {
+    if (i & 1) {
+        fruits[i].style.backgroundColor = "brown"
+        fruits[i].style.color = "white"
+    }
+}
+
